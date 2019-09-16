99050808ef329d8468b887b64010c3e5c85a6a32
Benjamin Camacho <benjamin.camacho@dabarc.com>
2019-09-16 15:46:17 -0500

    Se crea la estructura de proyecto

---
+++ src/Sakila.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sakila.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
