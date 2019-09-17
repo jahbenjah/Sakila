d6bb5d4f98dcb560c4b3e0c1c6cf894bbcdce3ff
Benjamin Camacho <benjamin.camacho@dabarc.com>
2019-09-16 20:16:35 -0500

    Se agrega el DbContext al proyecto Web

--- src/Sakila.Web/Controllers/HomeController.cs
+++ src/Sakila.Web/Controllers/HomeController.cs
@@ -4,15 +4,21 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Sakila.Infrastructure;
 using Sakila.Web.Models;
 
 namespace Sakila.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly SakilaContext _context;
+        public HomeController(SakilaContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            return View(_context.Actor.ToList());
         }
 
         public IActionResult Privacy()
--- src/Sakila.Web/Startup.cs
+++ src/Sakila.Web/Startup.cs
@@ -7,8 +7,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Sakila.Infrastructure;
 
 namespace Sakila.Web
 {
@@ -31,7 +33,7 @@ namespace Sakila.Web
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
+            services.AddDbContext<SakilaContext>(options => options.UseMySQL(Configuration.GetConnectionString("Sakila")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
